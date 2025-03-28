c12e98b691fe2147c5b5d8e8d1ce9af816ec8444
tim177 <timaraw18@gmail.com>
2025-03-28 22:12:18 +0530

    add handler

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -86,4 +86,5 @@ export const authOptions: NextAuthOptions = {
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export const GET = handler.handlers.GET;
+export const POST = handler.handlers.POST;
